4de818468b2f2aacb665adcb3f81170c40d59d60
Rowdife <76625609+Rowdife@users.noreply.github.com>
2020-12-29 06:50:17 +0900

    Update toDo.js

--- toDo.js
+++ toDo.js
@@ -2,9 +2,30 @@ const toDoForm = document.querySelector(".js-toDoForm"),
   toDoInput = toDoForm.querySelector("input"),
   toDoList = document.querySelector(".js-toDoList");
 
-const TODOS_LS = "toDos";
+const TODOS_LS = "toDos",
+  HIDE_TODOS = "hiding";
 
-const toDos = [];
+let toDos = [];
+
+function filterFn(toDo) {
+  return toDo.id === 1;
+}
+
+function deleteToDo(event) {
+  const btn = event.target;
+  const li = btn.parentNode;
+  toDoList.removeChild(li);
+  const cleanToDos = toDos.filter(function (toDo) {
+    return toDo.id !== parseInt(li.id);
+  });
+  toDos = cleanToDos;
+  saveToDos();
+}
+
+function clearToDo() {
+  localStorage.removeItem("toDos");
+  location.reload();
+}
 
 function saveToDos() {
   localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
@@ -17,7 +38,9 @@ function paintToDo(text) {
   const newId = toDos.length + 1;
 
   delBtn.innerText = "X";
+  delBtn.addEventListener("click", deleteToDo);
   span.innerText = text;
+
   li.appendChild(span);
   li.appendChild(delBtn);
   li.id = newId;
@@ -43,7 +66,7 @@ function loadToDos() {
   if (loadedToDos !== null) {
     const parsedToDos = JSON.parse(loadedToDos);
     parsedToDos.forEach(function (toDo) {
-      console.log(toDo.text);
+      paintToDo(toDo.text);
     });
   }
 }
